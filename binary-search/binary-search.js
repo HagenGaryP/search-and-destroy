8fb6a4fd1fa9034a39dbf14a3a8185c758406c04
Robu Waldorf <fieldassociationrecording@gmail.com>
2020-04-08 16:15:51 -0400

    isLoop complete

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,7 +2,14 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
+	let center = Math.floor(array.length / 2)
+	let midpoint = array[center]
+	console.log('center >>>', center);
+	console.log('midpoint>>>', midpoint)
+	if (midpoint === target) return true;
+	if (array.length === 1) return false;
+	if (target < midpoint)  return binarySearch(array.slice(0, center), target)
+	else return binarySearch(array.slice(center), target)
 };
 
 /*
--- looper/isloop.js
+++ looper/isloop.js
@@ -1,11 +1,17 @@
 'use strict';
-
+let values = [];
 //Complete this algo
+
+const nodeValGetter = (node) => {
+  if (values.includes(node.value))  return true;
+  if (!node.next) return false;
+  values.push(node.value);
+  return nodeValGetter(node.next);
+}
+
 const isLoop = (linkedlist) => {
-
+  return nodeValGetter(linkedlist.head);
 };
-
-
 /*
 EXTRA CREDIT:
 
@@ -15,3 +21,13 @@ Remember to write some test specs too!
 
 */
 module.exports = isLoop
+
+//variables
+//let headnode = linkedlist.head
+///headsnext = headnode.next
+
+// global array of previous values
+// if (globalarray includes current node's next value)  yes is a loop
+// if there next node is null or undefined 
+// if not, add current node's value to global array
+// recursively run function with current node's 'next' node
